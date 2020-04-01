ade985fb6e038cb18f287ca07a8476b77479283f
dimitrije97 <dimitrijesalic@yahoo.com>
2020-04-01 20:48:13 +0200

    Inicijaizacija projekta

---
+++ src/main/java/bezbednost/Bezbednost2020Application.java
@@ -0,0 +1,13 @@
+package bezbednost;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bezbednost2020Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bezbednost2020Application.class, args);
+	}
+
+}
