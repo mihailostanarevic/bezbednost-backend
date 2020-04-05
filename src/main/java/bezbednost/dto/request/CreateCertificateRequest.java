b9a15c8920d67618be55e10451b33f39c5f87fde
dimitrije97 <dimitrijesalic@yahoo.com>
2020-04-05 18:29:00 +0200

    Popunjena baza

---
+++ src/main/java/bezbednost/dto/request/CreateCertificateRequest.java
@@ -0,0 +1,7 @@
+package bezbednost.dto.request;
+
+import lombok.Data;
+
+@Data
+public class CreateCertificateRequest {
+}
---
+++ src/main/resources/data.sql
@@ -0,0 +1,2 @@
+insert into admin (id) values
+('a351022a-cc7a-4a17-a79b-8e0cf258db07');
