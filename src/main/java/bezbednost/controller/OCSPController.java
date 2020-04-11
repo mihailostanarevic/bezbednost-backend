667814fdc490a1e3321411863d1b0e2dd4f4470d
abeker <acabeker@gmail.com>
2020-04-12 00:46:56 +0200

    conflict resolved
    conflict resolved

--- src/main/java/bezbednost/controller/OCSPController.java
+++ src/main/java/bezbednost/controller/OCSPController.java
@@ -4,13 +4,16 @@ import bezbednost.service.IOCSPService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@SuppressWarnings("ALL")
 @RestController
 @RequestMapping("/ocsp")
 public class OCSPController {
+public class OcspController {
 
     private final IOCSPService _ocspService;
 
     public OCSPController(IOCSPService ocspService) {
+    public OcspController(IOCSPService ocspService) {
         _ocspService = ocspService;
     }
 }
